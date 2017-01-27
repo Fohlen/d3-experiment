ab619da589b3c9cece9d6010c58c5f3bbcbad74a
Fohlen <lennard@berger.work>
2017-01-27 22:28:31 +0100

    Add a very first chart, yeah!

--- api.js
+++ api.js
@@ -8,14 +8,21 @@ var api = new express.Router();
  * Gets all server countries with their counts
  */
 api.get('/server/countries', (req, res) => {
-
+  database.connect().then((Db) => {
+    let collection = Db.collection('games');
+    collection.aggregate([ { $group: {_id: "$country", total: { $sum: 1 } } } ], function (err, data) {
+      if (err) res.status(500).json(err);
+      res.json(data)
+      Db.close()
+    })
+  })
 })
 
 /**
  * Gets all player countries with their counts
  */
 api.get('/player/countries', (req, res) => {
-  
+
 })
 
 module.exports = api;
--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser')
-const browserify = require('browserify-middleware');
 const api = require('./api');
 
 var app = express();
 app.use(express.static('public'));
-app.use('/js/bundle.js', browserify(['d3'])) // this does not need bodyParser
 app.use(bodyParser.json())
 app.use('/api/', api);
 
