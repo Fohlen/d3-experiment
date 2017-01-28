58ca5fbc91280d85a6036d0b75841f57886b18a1
Fohlen <lennard@berger.work>
2017-01-28 15:48:50 +0100

    Add country by players.

--- api.js
+++ api.js
@@ -24,6 +24,26 @@ api.get('/server/countries', (req, res) => {
  * Gets all player countries with their counts
  */
 api.get('/player/countries', (req, res) => {
+  database.connect().then((Db) => {
+    let collection = Db.collection('games');
+    collection.aggregate([
+      { $unwind: "$players" },
+      { $group: {_id: "$players.country", total: { $sum: 1 } } },
+      { $sort: { total: -1 } }
+    ], function(err, data) {
+      if (err) res.status(500).json(err)
+      dataset = []
+      data.forEach((obj, index) => {
+        if (typeof(countries[obj._id]) != 'undefined') { // Somehow it happens that the sauertracker API cannot identify the contry for all players
+          obj._id = countries[obj._id].alpha3;
+          dataset.push(obj);
+        }
+      })
+
+      res.json(dataset)
+      Db.close()
+    })
+  })
 
 })
 
